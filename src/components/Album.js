5498e772d6e220ff78b68901c23488c3fca71d80
Derek Lundahl <dereklundahl@yahoo.com>
2018-07-16 10:11:51 -0600

    I added event listeners for mouse enter and mouse leave. right now they are just console loging.

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
 
 class Album extends Component {
   constructor(props) {
     super(props);
 
+  const playButton = <ion-icon name="play-circle"></ion-icon>;
+
+  const pauseButton = <ion-icon name="pause"></ion-icon>;
 
   const album = albumData.find( album => {
     return album.slug === this.props.match.params.slug
@@ -14,7 +17,8 @@ class Album extends Component {
   this.state = {
     album: album,
     currentSong: album.songs[0],
-    isPlaying: false
+    isPlaying: false,
+    displayButton: playButton
   };
   this.audioElement = document.createElement('audio');
   this.audioElement.src = album.songs[0].audioSrc
@@ -23,17 +27,20 @@ class Album extends Component {
 play() {
   this.audioElement.play();
   this.setState({ isPlaying: true });
+  this.setState({ displayButton: pauseButton });
 }
 
 pause() {
   this.audioElement.pause();
   this.setState({ isPlaying: false });
+  //this.setState({ displayButton: playButton });
 }
 
 setSong(song) {
   this.audioElement.src = song.audioSrc;
   this.setState({ currentSong: song });
 }
+
 handleSongClick(song) {
   const isSameSong = this.state.currentSong === song;
   if (this.state.isPlaying && isSameSong) {
@@ -42,6 +49,14 @@ handleSongClick(song) {
     if (!isSameSong) { this.setSong(song); }
     this.play();
   }
+}
+
+handleMouseEnter(song) {
+  console.log("the mouse has entered");
+}
+
+handleMouseLeave(song) {
+  console.log("the mouse has left");
 }
 
   render() {
@@ -65,14 +80,13 @@ handleSongClick(song) {
 
              {
                this.state.album.songs.map( (song, index) =>
-                 <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
-                   <ion-icon name="play"></ion-icon>
+                 <tr className="song" key={index} onClick={() => this.handleSongClick(song)} onMouseEnter={() => this.handleMouseEnter(song)} onMouseLeave={() => this.handleMouseLeave(song)} >
+                   <td>{this.state.displayButton}</td>
                    <td>{index + 1}</td>
                    <td>{song.title}</td>
                    <td>{song.duration}</td>
                  </tr>
-
-              )
+               )
              }
            </tbody>
         </table>
